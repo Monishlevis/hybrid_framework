b0448826ec80ba7cc2108b5a3a3b80bf11e3b010
Monishlevis <Monishv1@hexaware.com>
2026-06-02 14:26:40 +0530

    new commit

--- hybrid_framework/src/test/java/stepDefs/LoginSteps.java
+++ hybrid_framework/src/test/java/stepDefs/LoginSteps.java
@@ -1,42 +1,43 @@
 package stepDefs;
 
-import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
-import utils.DriverManager;
-import pages.LoginPage;
 import org.testng.Assert;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.LoginPage;
 import utils.ConfigReader;
-import utils.JsonReader;
+//import utils.JsonReader;
+import utils.DriverManager;
 
 public class LoginSteps {
 
     WebDriver driver = DriverManager.getDriver();
     LoginPage loginPage = new LoginPage(driver);
 
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+        driver.get(ConfigReader.getProperty("url"));
+    }
 
+    // @When("user enters username and password")
+    // public void user_enters_username_and_password() {
+    // loginPage.enterUsername(ConfigReader.getProperty("username"));
+    // loginPage.enterPassword(ConfigReader.getProperty("password"));
+    // }
 
-@Given("user is on login page")
-public void user_is_on_login_page() {
-    driver.get(ConfigReader.getProperty("url"));
-}
-
-// @When("user enters username and password")
-// public void user_enters_username_and_password() {
-//     loginPage.enterUsername(ConfigReader.getProperty("username"));
-//     loginPage.enterPassword(ConfigReader.getProperty("password"));
-// }
-
-
-@When("user enters username and password")
-public void user_enters_username_and_password() {
-    loginPage.enterUsername(ConfigReader.getProperty("username"));
-    loginPage.enterPassword(ConfigReader.getProperty("password"));
-}
+    @When("user enters username and password")
+    public void user_enters_username_and_password() {
+        loginPage.enterUsername(ConfigReader.getProperty("username"));
+        loginPage.enterPassword(ConfigReader.getProperty("password"));
+    }
 
     @And("clicks on login button")
     public void clicks_on_login_button() {
         loginPage.clickLogin();
-    }   
+    }
 
     @Then("user should be navigated to dashboard")
     public void user_should_be_navigated_to_dashboard() {
